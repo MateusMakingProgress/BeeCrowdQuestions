699f99c4c738715273dca86e7ba71bb09c015c4e
mateus de paula moreno <mateus.paula.moreno@gmail.com>
2025-01-12 10:35:16 -0300

    Substituição da pasta TipoDeCha por id BeeCrowd 2006

---
+++ Bee2006/Bee2006/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal class URI
+{
+    private static void Main(string[] args)
+    {
+
+        int tipoCha = int.Parse(Console.ReadLine());
+        string[] participantes = Console.ReadLine().Split(' ');
+        int acertos = 0;
+
+        for (int y = 0; y < participantes.Length; y++)
+        {
+            Console.WriteLine(participantes[y]);
+        }
+
+        for (int i = 0; i < participantes.Length; i++)
+        {
+            if (int.Parse(participantes[i]) == tipoCha)
+            {
+                acertos++;
+            }
+        }
+        Console.WriteLine(acertos);
+    }
+}
