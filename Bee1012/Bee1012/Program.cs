fc65bebddaf1aeb962898f60c851f927fd89be65
mateus de paula moreno <mateus.paula.moreno@gmail.com>
2025-01-12 15:48:22 -0300

    feat: add Bee1012

---
+++ Bee1012/Bee1012/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] abc = Console.ReadLine().Split(' ');
+
+        double areaA = double.Parse(abc[0]);
+        double areaB = double.Parse(abc[1]);
+        double areaC = double.Parse(abc[2]);
+
+        Console.WriteLine("TRIANGULO: " + ((areaA * areaC) / 2).ToString("F3"));
+        Console.WriteLine("CIRCULO: " +  (3.14159 * (areaC * areaC)).ToString("F3"));
+        Console.WriteLine("TRAPEZIO: " +  ((areaA + areaB) * areaC / 2).ToString("F3"));
+        Console.WriteLine("QUADRADO: " + (areaB * areaB).ToString("F3"));
+        Console.WriteLine("RETANGULO: " + (areaA * areaB).ToString("F3"));
+
+    }
+
+}
